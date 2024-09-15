c9ebe430279e57b9b24b3e3641d867363034f3bc
Luis Antunes <luisantunes0914@gmail.com>
2024-09-15 15:44:35 -0300

    add sequelize config and migrations

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,17 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      'Tudo certo': true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Luiz',
+      sobrenome: 'Antunes',
+      email: 'fdsf@fsdfsd.com',
+      idade: '20',
+      peso: '43',
+      altura: '2.4',
     });
+
+    res.json(novoAluno);
   }
 }
 
