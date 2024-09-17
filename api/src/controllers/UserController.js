b9946750f845fea135b03a38774d13bb981491cc
Luis Antunes <luisantunes0914@gmail.com>
2024-09-16 23:20:19 -0300

    add user crud and initial endpoints

---
+++ api/src/controllers/UserController.js
@@ -0,0 +1,93 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    try {
+      const novoUser = await User.create(req.body);
+      return res.json(novoUser);
+    } catch (e) {
+      return res.status(400).json({
+        error: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Id não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        error: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Id não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      const usuarioDeletado = await user.destroy(req.body);
+      return res.json(usuarioDeletado);
+    } catch (e) {
+      return res.status(400).json({
+        error: e.errors.map((err) => err.message),
+      });
+    }
+  }
+}
+
+export default new UserController();
+
+/*
+METODOS UTILIZADOS PELOS CONTROLLERS (NAO DEVE POSSUR MAIS QUE ISSO)
+- index -> lista todos os usuários -> GET
+- store/create -> cria um novo usuário -> POST
+- delete -> apaga um usuário -> DELETE 
+- show -> mostra um usuário -> GET 
+- update -> atualiza um usuário -> PATCH ou PUT
+*/
