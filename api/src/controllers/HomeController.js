2ae76896b0ac997b3808e3206b640251e6f4b693
Luis Antunes <luisantunes0914@gmail.com>
2024-09-19 22:12:06 -0300

    add students route

--- api/src/controllers/HomeController.js
+++ api/src/controllers/HomeController.js
@@ -1,17 +1,6 @@
-import aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoaluno = await aluno.create({
-      nome: 'luiz',
-      sobrenome: 'antunes',
-      email: 'fdsf@fsdfsd.com',
-      idade: '20',
-      peso: '43',
-      altura: '2.4',
-    });
-
-    res.json(novoaluno);
+    res.json('Index');
   }
 }
 
